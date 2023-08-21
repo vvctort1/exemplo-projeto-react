917187b9b98be52f0f7434d6c96b927129702468
vvctort1 <rm551649@fiap.com.br>
2023-08-21 10:05:23 -0300

    menu.jsx criado e app.jsx alterado

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+
+export default function Menu() {
+  return (
+  
+    <ul>
+        <li>FIAP</li>
+        <li>GOOGLE</li>
+        <li>FACEBOOK</li>
+        <li>X</li>
+        <li>Amazon</li>
+    </ul>
+  )
+}
